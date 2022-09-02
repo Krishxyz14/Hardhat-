f33abeab82016473c6e46b1ee57dac5c52c0972a
kri <krishxyz14@gmail.com>
2022-09-02 15:41:02 +0530

    Hardhat by use of Infura

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,15 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 require("@nomiclabs/hardhat-waffle")
 
-const ALCHEMY_API_KEY="itiTFZLFgi3z8xnwrzTyXPD-MZ3uyNF8";
-const RINKEBY_PRIVATE_KEY="1a4278eff114ebe8fc745096f6342b3d6c60ca7122f0c8fc4db343715baff4df";
+const INFURA_API_KEY="7a308ec8442040c292c46baac51ddd6b";
+const ROPSTEN_PRIVATE_KEY="1a4278eff114ebe8fc745096f6342b3d6c60ca7122f0c8fc4db343715baff4df";
 module.exports = {
   solidity: "0.8.9",
 
   networks:{
-    rinkeby:{
-      url:`https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
-      accounts:[`${RINKEBY_PRIVATE_KEY}`]
+    ropsten:{
+      url:`https://ropsten.infura.io/v3/${INFURA_API_KEY}`,
+      accounts:[`${ROPSTEN_PRIVATE_KEY}`]
     }
   }
 };
