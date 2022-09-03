92a4365784a94256e6b172717f85d785afcc9e7e
Krishxyz14 <111213463+Krishxyz14@users.noreply.github.com>
2022-09-03 15:11:10 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,7 @@
 require("@nomiclabs/hardhat-waffle")
 
 const INFURA_API_KEY="7a308ec8442040c292c46baac51ddd6b";
-const ROPSTEN_PRIVATE_KEY="1a4278eff114ebe8fc745096f6342b3d6c60ca7122f0c8fc4db343715baff4df";
+const ROPSTEN_PRIVATE_KEY="1a4278eff****************************************";
 module.exports = {
   solidity: "0.8.9",
 
